0300aab9adfd88eacba05a6cd3309521ba300dcb
Oriol Miro <omiro@xtec.cat>
2024-12-11 17:27:18 +0100

    Add updateExchange method to CurrencyController for fetching and updating exchange rates

--- app/Http/Controllers/CurrencyController.php
+++ app/Http/Controllers/CurrencyController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Currency;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class CurrencyController extends Controller
 {
@@ -30,5 +32,41 @@ class CurrencyController extends Controller
             'to_currency' => $targetCurrency,
             'amount' => $amount,
         ]);
+    }
+
+    public function updateExchange()
+    {
+        // Realizar la solicitud a la API para obtener los valores de cambio
+        $response = Http::get('https://api.currencyapi.com/v3/latest', [
+            'apikey' => env('CURRENCY_API'),
+        ]);
+    
+        // Verificamos si la respuesta es exitosa
+        if ($response->successful()) {
+            $data = $response->json()['data']; // Obtenemos los datos de la respuesta
+    
+            // Iterar sobre las monedas obtenidas y actualizar los valores
+            foreach ($data as $currencyCode => $currency) {
+                $currencyRecord = Currency::where('code', $currencyCode)->first();
+    
+                // Si la moneda existe, actualizamos el valor
+                if ($currencyRecord) {
+                    $currencyRecord->value = $currency['value'];
+                    $currencyRecord->save();
+                }
+            }
+    
+            // Retornar una respuesta exitosa
+            return response()->json([
+                'status' => 'ok',
+                'message' => 'Currency exchange rates updated successfully.',
+            ]);
+        } else {
+            // Si la solicitud falla, se retorna un error
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Failed to update currency exchange rates.',
+            ], 500);
+        }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,4 @@ Route::get('/convert', function () {
 })->name('convert.index');
 Route::get('/convert/{amount}/{from_currency}/{to_currency}', [CurrencyController::class, 'convertCurrency'])->name('convert.result');
 Route::post('/convert', [CurrencyController::class, 'convert'])->name('convert');
+Route::get('/convert/updateExchange', [CurrencyController::class, 'updateExchange'])->name('convert.update');
